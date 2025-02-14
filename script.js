58c84bdabab5608f184d6942a80cc19a64dcfa56
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-14 12:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 let codeInnerHTML = "";
 
 for (let i = 1; i <= 6; i++) {
-    codeInnerHTML += `<input class="code code-${i}" maxlength="1" placeHolder="0" onkeydown="move(event, ${i})">`;
+    codeInnerHTML += `<input id="code-${i}" class="code" maxlength="1" placeHolder="0" onkeydown="move(event, ${i})">`;
 }
 
 document.querySelector(".code-container").innerHTML = codeInnerHTML;
 
 function move(event, idx){
-	const element = document.querySelector(`.code-${idx}`);
+	const element = document.querySelector(`#code-${idx}`);
 
 	if (event.key == "Backspace" && element.value.length == 0 && idx > 0){
-		document.querySelector(`.code-${idx-1}`).focus();
+		document.querySelector(`#code-${idx-1}`).focus();
 	}
 
 	if (element.value.length == 1 && idx < 6){
-		document.querySelector(`.code-${idx+1}`).focus();
+		document.querySelector(`#code-${idx+1}`).focus();
 	}
 }
 

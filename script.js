20cdbe6fd7d2fc5dfab07fba6cd0f0ea775a6b1f
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-14 12:02:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+// Initialize an empty string for codeInnerHTML
+let codeInnerHTML = "";
+
+for (let i = 1; i <= 6; i++) {
+    codeInnerHTML += `<input class="code code-${i}" maxlength="1" placeHolder="0" onkeydown="move(event, ${i})">`;
+}
+
+document.querySelector(".code-container").innerHTML = codeInnerHTML;
+
+function move(event, idx){
+	const element = document.querySelector(`.code-${idx}`);
+
+	if (event.key == "Backspace" && element.value.length == 0 && idx > 0){
+		document.querySelector(`.code-${idx-1}`).focus();
+	}
+
+	if (element.value.length == 1 && idx < 6){
+		document.querySelector(`.code-${idx+1}`).focus();
+	}
+}
+
+
